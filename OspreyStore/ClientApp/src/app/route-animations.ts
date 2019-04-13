dc80b8949dc74e569372b02c80dff214171d5b86
Александр Козаченко <osprey.alexandr@gmail.com>
2019-04-13 11:32:20 +0300

    animations work.. so far

--- OspreyStore/ClientApp/src/app/route-animations.ts
+++ OspreyStore/ClientApp/src/app/route-animations.ts
@@ -9,8 +9,8 @@ import {
   keyframes,
 } from '@angular/animations';
 
-export const slideInAnimation =
-  trigger('routeAnimations', [
+//export const slideInAnimation =
+
     //transition('HomePage <=> AboutPage', [
     //  style({ position: 'relative' }),
     //  query(':enter, :leave', [
@@ -35,7 +35,69 @@ export const slideInAnimation =
     //  ]),
     //  query(':enter', animateChild()),
     //]),
-    transition('* <=> *', [
+//  trigger('routeAnimations', [
+  //  transition('* => *', [
+  //    style({ position: 'relative' }),
+  //    query(':enter, :leave', [
+  //      style({
+  //        position: 'absolute',
+  //        top: 0,
+  //        left: 0,
+  //        width: '100%'
+  //      })
+  //    ]),
+  //    query(':enter', [
+  //      style({ left: '-100%' })
+  //    ]),
+  //    query(':leave', animateChild()),
+  //    group([
+  //      query(':leave', [
+  //        animate('200ms ease-out', style({ left: '100%' }))
+  //      ]),
+  //      query(':enter', [
+  //        animate('300ms ease-out', style({ left: '0%' }))
+  //      ])
+  //    ]),
+  //    query(':enter', animateChild()),
+  //  ])
+  //]);
+
+export const slideInAnimation =
+  trigger('routeAnimations', [
+    transition('* => *', slideTo('left')),
+    //transition('* => isRight', slideTo('right')),
+    //transition('isRight => *', slideTo('left')),
+    //transition('isLeft => *', slideTo('right'))
+  ]);
+
+function slideTo(direction) {
+  const optional = { optional: true };
+  return [
+    query(':enter, :leave', [
+      style({ position: 'relative', })
+    ], optional),
+    query(':enter', [
+      style({ left: '-100%' })
+    ], optional),
+    query(':leave', animateChild(), optional),
+    group([
+      query(':leave', [
+        animate('300ms ease-out', style({ left: '100%' }))
+      ], optional),
+      query(':enter', [
+        animate('400ms ease-out', style({ left: '0%' }))
+      ], optional)
+    ]),
+    query(':enter', animateChild(), optional),
+    // Normalize the page style... Might not be necessary
+
+    // Required only if you have child animations on the page
+     //query(':leave', animateChild()),
+     //query(':enter', animateChild()),
+  ];
+}
+  trigger('routeAnimations', [
+    transition('* => *', [
       style({ position: 'relative' }),
       query(':enter, :leave', [
         style({
@@ -60,41 +122,3 @@ export const slideInAnimation =
       query(':enter', animateChild()),
     ])
   ]);
-
-//export const slider =
-//  trigger('routeAnimations', [
-//    transition('* => isLeft', slideTo('left')),
-//    transition('* => isRight', slideTo('right')),
-//    transition('isRight => *', slideTo('left')),
-//    transition('isLeft => *', slideTo('right'))
-//  ]);
-
-//function slideTo(direction) {
-//  const optional = { optional: true };
-//  return [
-//    query(':enter, :leave', [
-//      style({
-//        position: 'relative',
-//        top: 0,
-//        //[direction]: 0,
-//        //width: '100%'
-//      })
-//    ], optional),
-//    query(':enter', [
-//      style({ [direction]: '-100%' })
-//    ]),
-//    group([
-//      //query(':leave', [
-//      //  animate('500ms ease', style({ [direction]: '100%' }))
-//      //], optional),
-//      query(':enter', [
-//        animate('500ms ease', style({ [direction]: '0%' }))
-//      ])
-//    ]),
-//    // Normalize the page style... Might not be necessary
-
-//    // Required only if you have child animations on the page
-//     //query(':leave', animateChild()),
-//     //query(':enter', animateChild()),
-//  ];
-//}
