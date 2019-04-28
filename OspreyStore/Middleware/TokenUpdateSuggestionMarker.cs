6aa4018bbc43b8d0fd1c39befb77a0d8497d3e90
Александр Козаченко <osprey.alexandr@gmail.com>
2019-04-28 12:27:36 +0300

    solid readme

--- OspreyStore/Middleware/TokenUpdateSuggestionMarker.cs
+++ OspreyStore/Middleware/TokenUpdateSuggestionMarker.cs
@@ -1,53 +1,53 @@
-﻿using ApplicationCore.Identity;
-using Infrastructure.Identity;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
+﻿//using ApplicationCore.Identity;
+//using Infrastructure.Identity;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Identity;
+//using System;
+//using System.Collections.Generic;
+//using System.IdentityModel.Tokens.Jwt;
+//using System.Linq;
+//using System.Security.Claims;
+//using System.Threading.Tasks;
 
-namespace OspreyStore.Middleware
-{
-    public class TokenUpdateSuggestionMarker
-    {
-        private readonly RequestDelegate next;
+//namespace OspreyStore.Middleware
+//{
+//    public class TokenUpdateSuggestionMarker
+//    {
+//        private readonly RequestDelegate next;
 
-        public TokenUpdateSuggestionMarker(RequestDelegate next)
-        {
-            this.next = next;
-        }
+//        public TokenUpdateSuggestionMarker(RequestDelegate next)
+//        {
+//            this.next = next;
+//        }
 
-        public async Task Invoke(HttpContext context, UserManager<ApplicationUser> userManager)
-        {
-            context.Response.OnStarting(async state =>
-            {
-                string tokenString = GetTokenFromAuthorizationString(context.Request.Headers["Authorization"]);
-                var tokenHandler = new JwtSecurityTokenHandler();
-                if (tokenHandler.CanReadToken(tokenString) && context.User != null)
-                {
-                    var token = tokenHandler.ReadJwtToken(tokenString);
-                    //var id = token.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Name) ?? throw new Exception($"Name claim not found at token {tokenString}");
-                    var user = await userManager.FindByIdAsync(token.Subject) ?? throw new Exception($"User {token.Subject} not found");
-                    if (user.ClaimsUpdatedAt > token.ValidFrom)
-                    {
-                        var httpContext = (HttpContext)state;
-                        httpContext.Response.Headers.Add("Claims-Changed", "true");
-                    }
-                }
-            }, context);
-            await next(context);
-        }
+//        public async Task Invoke(HttpContext context, UserManager<ApplicationUser> userManager)
+//        {
+//            context.Response.OnStarting(async state =>
+//            {
+//                string tokenString = GetTokenFromAuthorizationString(context.Request.Headers["Authorization"]);
+//                var tokenHandler = new JwtSecurityTokenHandler();
+//                if (tokenHandler.CanReadToken(tokenString) && context.User != null)
+//                {
+//                    var token = tokenHandler.ReadJwtToken(tokenString);
+//                    //var id = token.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Name) ?? throw new Exception($"Name claim not found at token {tokenString}");
+//                    var user = await userManager.FindByIdAsync(token.Subject) ?? throw new Exception($"User {token.Subject} not found");
+//                    if (user.ClaimsUpdatedAt > token.ValidFrom)
+//                    {
+//                        var httpContext = (HttpContext)state;
+//                        httpContext.Response.Headers.Add("Claims-Changed", "true");
+//                    }
+//                }
+//            }, context);
+//            await next(context);
+//        }
 
-        private string GetTokenFromAuthorizationString(string authorization)
-        {
-            string result = string.Empty;
-            if (authorization != null && authorization.Length > 8)
-                result = authorization.Substring(7);
-            //int bearerPrefixLenght = 7;
-            return result;
-        }
-    }
-}
+//        private string GetTokenFromAuthorizationString(string authorization)
+//        {
+//            string result = string.Empty;
+//            if (authorization != null && authorization.Length > 8)
+//                result = authorization.Substring(7);
+//            //int bearerPrefixLenght = 7;
+//            return result;
+//        }
+//    }
+//}
--- README.md
+++ README.md
@@ -31,14 +31,22 @@ Each component designed for one thing and has minimal knowledge of outside compo
 Generally the process of creating an entity is similar between different services 
 ![OpenClosedPrinciple1](docs/images/OpenClosedPrinciple1.jpg)
 
-When we need to add file to storage while creating an entity (ItemVariantImageService), this general process could be extended easily
+When we need to add file to storage while creating an entity (ItemVariantImageService), this general process could be easily extended while still keeping and reusing the original logic
 ![OpenClosedPrinciple2](docs/images/OpenClosedPrinciple2.jpg)
 
-
+#### L-Liskov substitution principle
 Controllers rely on services to create/read/update/delete entities. Although the internal logic may differ greatly from service to service, in the end it's just generic controller calling generic service while passing some arguments.
 ![LiskovSubstitutionPrinciple1](docs/images/LiskovSubstitutionPrinciple1.jpg)
 ![LiskovSubstitutionPrinciple2](docs/images/LiskovSubstitutionPrinciple2.jpg)
 
+#### I-Interface segregation principle
+As already mentioned, service may rely on other services, but only does so through interfaces. In this example, ItemService requires ICategoryService to filter by category and IItemVariantImageService to maintain database consistency.
+![InterfaceSegregationPrinciple1](docs/images/InterfaceSegregationPrinciple1.jpg)
+
+#### D-Dependency inversion principle
+Accrording to Clean Architecture principles, the most important one is the dependancy inversion principle which is the pillar of the clean architecture.
+While there are still some references from OspreyStore to Infrastructure, it's only limited to database seeding and DI setup, as it's shown on a graph below.
+![DependencyInversionPrinciple1](docs/images/DependencyInversionPrinciple1.jpg)
 
 
 ### Installing
